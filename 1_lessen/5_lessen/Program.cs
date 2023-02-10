2011ab26a7b18eb030a4f3a9889a4755079437d3
VasyaKhokhlov <vvkhokhlov@hotmail.com>
2023-02-10 16:04:13 +0300

    10.02.2023_16.04

---
+++ 1_lessen/5_lessen/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 4;
+int b = 5;
+int c = 2;
+int d = 7;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+
+Console.Write("max =");
+Console.WriteLine(max);
