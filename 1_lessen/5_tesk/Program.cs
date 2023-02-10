58a5bae0888f3d224812a9c8f6c5b678c3d67b7a
VasyaKhokhlov <vvkhokhlov@hotmail.com>
2023-02-10 16:57:24 +0300

    10.02.2023_16.57

---
+++ 1_lessen/5_tesk/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 5;
+int c = 4;
+int d = 10;
+
+int min = a;
+
+if (a<min) min = a;
+if (b<min) min = b;
+if (c<min) min = c;
+if (d<min) min = d;
+
+Console.Write("min =");
+Console.WriteLine(min);
