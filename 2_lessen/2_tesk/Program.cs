3b37ec3b52732d6bb99ff4c8614902576d3fba92
VasyaKhokhlov <vvkhokhlov@hotmail.com>
2023-02-11 23:07:18 +0300

    11.02.2023_23.07

---
+++ 2_lessen/2_tesk/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{Math.Pow(b,2) == a}");
+if(Math.Pow(b,2) == a)
+{
+    Console.WriteLine("а квадрат b");
+}else
+{
+    Console.WriteLine("а не квадрат b");
+}
