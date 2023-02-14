9941b81099b14f04eaa0580fe4bc1ee334a558c7
VasyaKhokhlov <vvkhokhlov@hotmail.com>
2023-02-14 13:45:07 +0300

    2

---
+++ Home_2/Home_1_task/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int stg = int.Parse(Console.ReadLine());
+int a1 = stg;
+if (stg<=999 && stg>99)
+{
+    a1 = (stg / 10) %10;
+    Console.WriteLine(a1);
+} else
+{
+    if (stg>999 || stg<99)
+    {
+        Console.WriteLine("Введите трехзначное число");
+    }
+}
+
+
---
+++ Home_2/Home_3_task/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int stg = int.Parse(Console.ReadLine());
+int a1 = stg;
+if (a1 > 0 && a1 < 6){
+    Console.WriteLine("НЕТ");
+}else
+{
+    if (a1>5 && a1<8)
+    {
+        Console.WriteLine("ДА");
+ 
+    }else
+    {
+        Console.WriteLine("Ввели не день недели");
+    }
+}
+
+ 
+
