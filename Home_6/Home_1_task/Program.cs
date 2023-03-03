0417375e3f367919b9136a4652a59f690c065616
VasyaKhokhlov <vvkhokhlov@hotmail.com>
2023-03-04 00:41:46 +0300

    DZ6

---
+++ Home_6/Home_1_task/Program.cs
@@ -0,0 +1,29 @@
+﻿/* **Задача 41:** Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+
+int[] GetArray(string text)
+{
+    string[] str = text.Split(" ");
+    int[] array = new int[str.Length];
+    for (int i = 0; i < str.Length; i++){
+        array[i] = int.Parse(str[i]);
+    }return array;
+}
+
+// Метод 
+int Count(int[] array)
+{
+    int count = 0;
+    foreach (int el in array){
+        if (el > 0) count++;
+    }return count;
+}
+
+Console.Clear();
+Console.Write($"Введите элементы массива через пробел: ");
+int[] array = GetArray(Console.ReadLine());
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"Чисел больше 0 -> {Count(array)}");
---
+++ Home_6/Home_2_task/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 43:** Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+/*Если подставить y, то k1*x+b1 = k2*x+b2 -> k1*x-k2*x = b2-b1 -> x(k1-k2) = b2-b1 -> x = b2-b1 / k1-k2 
+y = k2 * x + b2*/
+
+double[] InputData(string text)
+{
+    Console.Write(text);
+    string[] str = text.Split(" ");
+    double[] num = new double[2];
+    num[0] = double.Parse(str[0]);
+    num[1] = double.Parse(str[1]);
+    Console.WriteLine();
+    return num;
+}
+
+double[] XandY(double[] array1, double[] array2)
+{
+    double[] coordinate = new double[2];
+    coordinate[0] = (array2[0] - array1[0]) / (array1[1] - array2[1]); // 0 - b, 1 - k
+    coordinate[1] = array2[1] * coordinate[0] + array2[0];
+    return coordinate;
+}
+
+bool Validate(double[] array1, double[] array2)
+{    
+    if (array1[0] == array2[0] && array1[1] == array2[1]){
+        Console.WriteLine("Прямые совпадают");
+        return false;
+    }
+    if (array1[0] == array2[0] || array1[1] == array2[1]){
+        Console.WriteLine("Прямые паралельны");
+        return false;
+    }
+return true;
+}
+
+Console.Clear();
+Console.Write("Введите значения b и k для прямой 1: ");
+double[] Line1 = InputData(Console.ReadLine());
+
+Console.Write("Введите значения b и k для прямой 2: ");
+double[] Line2 = InputData(Console.ReadLine());
+if (Validate(Line1, Line2))
+{
+    double[] coordinate = XandY(Line1, Line2);
+    Console.Write($"Точка пересечения уравнений прямых y={Line1[1]}*x+{Line1[0]} и y={Line2[1]}*x+{Line2[0]} ");
+    Console.Write($"Имеет координаты ({String.Join(", ", coordinate)}) ");
+}
